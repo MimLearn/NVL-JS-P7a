962c6cd7a5499b33d7906ed18bc6791ac018d8c2
MimLearn <51676988+MimLearn@users.noreply.github.com>
2019-06-25 11:39:05 +0200

    p7a.js

---
+++ p7a.js
@@ -0,0 +1,6 @@
+<script>
+var amigo = 'Amigo';
+var amigoLowerCase_1 = amigo.toLowerCase();
+var amigoLowerCase_2 = amigo.replace(amigo[0],'a');
+var amigoLowerCase_3 = amigo.replace('A','a');
+</script>
